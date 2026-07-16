e95bd7d9b299f64a4955eb9833d6406b80e217f6
Brad <ssjbrad@users.noreply.github.com>
2026-07-16 16:18:46 +1200

    basic data strutures created

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
-fn main() {
-    println!("Hello, world!");
+use std::collections::HashMap;
+
+fn main() {}
+
+//Basic main data structures
+
+//User accounts
+struct User {
+    balance: u64,
+    nonce: u64,
+}
+
+//Global state
+struct State {
+    global_state: HashMap<String, User>,
+}
+
+//Transaction requests
+struct Transaction {
+    receiver_address: String,
+    sender_address: String,
+    signer: Vec<u8>,
+    amount: u64,
+}
+
+//A block of transaction requests
+struct Block {
+    transactions: Vec<Transaction>,
+    previous_hash: String,
+    hash: String,
+    timestamp: u64, //unix time, seconds since Jan 1 1970
+}
+
+//All transaction requests, chained.
+struct Blockchain {
+    blocks: Vec<Block>,
 }
